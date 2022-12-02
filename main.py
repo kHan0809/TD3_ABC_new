100f6ab33fea84804f2e0a41d165abd615465802
kHan0809 <kimhangyeol14@gmail.com>
2022-12-02 16:44:58 +0900

    .
    .

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
 	parser.add_argument("--seed", default=1, type=int)              # Sets Gym, PyTorch and Numpy seeds
 	parser.add_argument("--eval_freq", default=5e3, type=int)       # How often (time steps) we evaluate
 	parser.add_argument("--max_timesteps",   default=1e6, type=int)   # Max time steps to run environment
-	parser.add_argument("--max_timesteps_Q", default=1e4, type=int, help="3e4")  # Max time steps to run environment
+	parser.add_argument("--max_timesteps_Q", default=3e4, type=int, help="3e4")  # Max time steps to run environment
 	parser.add_argument("--save_model", action="store_true")        # Save model and optimizer parameters
 	parser.add_argument("--load_model", default="")                 # Model load file name, "" doesn't load, "default" uses file_name
 	# TD3
